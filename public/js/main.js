b2aee22c2973f23fb32ff7639797ef0af542accb
zulma <zumendez7@gmail.com>
2020-12-29 09:55:46 +0100

    exo 1 et 2 terminés

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+// exo1
+let chiffre = 7;
+let chiffre2 = 33;
+
+console.log(`${chiffre} + ${chiffre2} = ${chiffre + chiffre2}`) ;
+console.log(`${chiffre} - ${chiffre2} = ${chiffre - chiffre2}`) ;
+console.log(`${chiffre} * ${chiffre2} = ${chiffre * chiffre2}`) ;
+console.log(`${chiffre} / ${chiffre2} = ${chiffre / chiffre2}`) ;
+
+
+// exo2
+let age = parseInt(prompt('quel est votre age?'));
+let prenom = prompt('que est votre prenom?');
+
+// console.log(`${prenom} a ${age} ans cette année`)
+// console.log(typeof age)
+
+console.log(`${age + 3}`) ;
+
+
+
